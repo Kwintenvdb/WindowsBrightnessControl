c5a9724a09b9efeb83bc1a0830f70184806adea3
kwintenvdb <kwintenvdberghe@gmail.com>
2018-06-26 23:28:58 +0200

    Added some more UI to settings

--- WindowsBrightnessControl/Model/Settings.cs
+++ WindowsBrightnessControl/Model/Settings.cs
@@ -3,13 +3,12 @@
 	// Use application settings? Might be easier.
 	public class Settings
 	{
-		public bool UseHotKeys { get; set; } = true;
-
 		// Use intervals?
 		public bool SnapBrightness { get; set; } = true;
 		public int SnappingInterval { get; set; } = 5;
 
 		// Application settings
 		public bool RunOnStartUp { get; set; } = true;
+		public bool UseHotKeys { get; set; } = true;
 	}
 }
