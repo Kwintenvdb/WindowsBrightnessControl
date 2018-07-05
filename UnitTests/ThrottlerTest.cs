178e74d79ed5ed66dc76378651d3c7940a591eda
kwintenvdb <kwintenvdberghe@gmail.com>
2018-07-05 19:32:41 +0200

    Added unit testing project.

---
+++ UnitTests/ThrottlerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WindowsBrightnessControl.Util;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class ThrottlerTest
+	{
+		[Test]
+		public void ThrottleFunction()
+		{
+			var throttler = new Throttler(1.0);
+
+			int counter = 0;
+			Action action = () =>
+			{
+				counter++;
+			};
+
+			bool result = throttler.TryExecute(action);
+			Assert.IsTrue(result);
+			result = throttler.TryExecute(action);
+			Assert.IsFalse(result);
+
+			Task.Delay(2000).Wait();
+			result = throttler.TryExecute(action);
+			Assert.IsTrue(result);
+
+			Assert.AreEqual(2, counter);
+		}
+	}
+}
---
+++ UnitTests/ValueViewModelTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using WindowsBrightnessControl.ViewModel;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class ValueViewModelTest
+	{
+		[Test]
+		public void AssignValue()
+		{
+			int value = 10;
+			var vm = new ValueViewModel<int>(value);
+
+			Assert.AreEqual(value, vm.Value);
+
+			int newValue = 20;
+			vm.Value = newValue;
+			Assert.AreEqual(newValue, vm.Value);
+		}
+
+		[Test]
+		public void HandleChangedValue()
+		{
+			int value = 10;
+			int changedValue = 20;
+			var vm = new ValueViewModel<int>(value);
+
+			int numValueChanged = 0;
+
+			vm.ValueChanged += (newValue) =>
+			{
+				Assert.AreEqual(newValue, changedValue);
+				++numValueChanged;
+				Assert.AreEqual(1, numValueChanged);
+			};
+
+			vm.Value = changedValue;
+			// ValueChanged should not be raised if value is unchanged.
+			vm.Value = changedValue;
+		}
+	}
+}
